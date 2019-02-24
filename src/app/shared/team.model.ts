8b3cbbadc10efad095f0ba3ca550fe4a021a6c72
akosborn <andrewosborn93@gmail.com>
2019-02-24 12:06:25 -0500

    Added bubble property to team and metrics-difference

--- src/app/shared/team.model.ts
+++ src/app/shared/team.model.ts
@@ -5,13 +5,14 @@ export class Team {
   id: number;
   name: string;
   conference: Conference;
+  isOnBubble: boolean;
   metrics: Array<Metrics>;
 
-
-  constructor(id: number, name: string, conference: Conference, metrics: Array<any>) {
+  constructor(id: number, name: string, conference: Conference, isOnBubble: boolean, metrics: Array<any>) {
     this.id = id;
     this.name = name;
     this.conference = conference;
+    this.isOnBubble = isOnBubble;
     this.metrics = metrics;
   }
 }
